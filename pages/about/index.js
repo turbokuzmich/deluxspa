de0d644623b9fc533f78a4de60d289362c970606
Kurteev Dmitry <kurteev.d@yandex.ru>
2022-08-02 11:20:36 +0300

    About

--- pages/about/index.js
+++ pages/about/index.js
@@ -12,52 +12,84 @@ export default function About() {
         <Container>
           <Box
             sx={{
-              pt: {
-                xs: 4,
+              gap: {
                 md: 8,
               },
-              pb: {
-                xs: 4,
-                md: 8,
-              },
-              width: {
-                md: 800,
+              display: {
+                md: "flex",
               },
             }}
           >
-            <Typography
-              variant="h3"
-              sx={{ textTransform: "uppercase" }}
-              paragraph
+            <Box
+              sx={{
+                pt: {
+                  xs: 4,
+                  md: 8,
+                },
+                pb: {
+                  xs: 4,
+                  md: 8,
+                },
+              }}
             >
-              DeluxSPA
-            </Typography>
-            <Typography variant="h5" paragraph>
-              Профессиональный путь к красоте
-            </Typography>
-            <Typography>
-              ООО «Демидов Люкс СПА» является участником внешнеэкономической
-              деятельности с 2020 года, представляя на зарубежных рынках
-              продукцию высокого качества. Торговые марки Delux SPA,{" "}
-              <Link href="https://yoko.ru" passHref>
-                <A target="_blank">Yoko</A>
-              </Link>
-              ,{" "}
-              <Link href="https://neon-beard.ru" passHref>
-                <A target="_blank">Neon Beard</A>
-              </Link>
-              ,{" "}
-              <Link href="https://sporthit.online" passHref>
-                <A target="_blank">Sport Hit</A>
-              </Link>
-              ,{" "}
-              <Link href="https://stopvirus.moscow" passHref>
-                <A target="_blank">NEW//BREEZE</A>
-              </Link>{" "}
-              представлены на рынках стран СНГ, а также азиатских стран. Наши
-              международные партнеры – это успешные и быстро развивающиеся
-              компании. Всегда рады сотрудничеству.
-            </Typography>
+              <Typography
+                variant="h3"
+                sx={{ textTransform: "uppercase" }}
+                paragraph
+              >
+                DeluxSPA
+              </Typography>
+              <Typography variant="h5" paragraph>
+                Профессиональный путь к красоте
+              </Typography>
+              <Typography paragraph>
+                ООО «Демидов Люкс СПА» является участником внешнеэкономической
+                деятельности с 2020 года, представляя на зарубежных рынках
+                продукцию высокого качества.
+              </Typography>
+              <Typography paragraph>
+                ООО «Демидов Люкс СПА» в{" "}
+                <Link href="https://i.moscow/company/1862605" passHref>
+                  <A target="_blank">Московском инновационном кластере</A>
+                </Link>{" "}
+                и{" "}
+                <Link href="https://moscow-export.com/" passHref>
+                  <A target="_blank">Московском экспортном центре</A>
+                </Link>
+                .
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                pt: {
+                  md: 8,
+                },
+                pb: {
+                  xs: 4,
+                  md: 8,
+                },
+                width: {
+                  md: 300,
+                },
+                flexShrink: 0,
+              }}
+            >
+              <Typography
+                variant="h4"
+                sx={{ textTransform: "uppercase" }}
+                paragraph
+              >
+                Реквизиты
+              </Typography>
+              <Typography>ООО «Демидов Люкс СПА»</Typography>
+              <Typography>р/с 40702810200000000401</Typography>
+              <Typography>Кор/с 30101810200000000700</Typography>
+              <Typography>БИК 044525700</Typography>
+              <Typography>ИНН 7751525117</Typography>
+              <Typography>КПП 775001001</Typography>
+              <Typography>ОКПО 42943661</Typography>
+              <Typography>ОГРН 5147746230297</Typography>
+            </Box>
           </Box>
         </Container>
       </>
