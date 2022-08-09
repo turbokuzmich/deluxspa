62dbdc0bf89e5818178bd19b3b32f0aad9163209
Kurteev Dmitry <kurteev.d@yandex.ru>
2022-08-09 18:21:08 +0300

    html email letter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,38 +1,170 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 import adapter from "../../../lib/adapters/strapi";
-// import SequelizeAdapter from "@next-auth/sequelize-adapter";
-// import { Sequelize } from "sequelize";
-
-// const sequelize = new Sequelize({
-//   dialect: "sqlite",
-//   storage: "./temp.db",
-//   logging: false,
-// });
-// const adapter = new SequelizeAdapter(sequelize);
-
-// sequelize.sync();
+import nodemailer from "nodemailer";
 
 export default NextAuth({
   adapter: adapter,
-  secret: process.env.SESSION_SECRET,
   session: {
     strategy: "jwt",
   },
   providers: [
     EmailProvider({
       from: process.env.EMAIL_USER,
-      secret: process.env.SESSION_SECRET,
+      secret: process.env.NEXTAUTH_SECRET,
       server: {
         host: "smtp.yandex.ru",
         port: 465,
         secure: true,
         auth: {
-          type: "login",
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
         },
       },
+      async sendVerificationRequest({ identifier, url, expires, provider }) {
+        // console.log(identifier);
+        // console.log(url);
+        // console.log(expires);
+        const transport = nodemailer.createTransport(provider.server);
+
+        const result = await transport.sendMail({
+          to: identifier,
+          from: provider.from,
+          sender: process.env.EMAIL_SENDER,
+          subject: `Авторизация на сайте DeluxSPA`,
+          text: `Для авторизации на сайте DeluxSPA, пожалуйста, перейдите по ссылке ${url}`,
+          html: html(url),
+        });
+
+        const failed = result.rejected.concat(result.pending).filter(Boolean);
+        if (failed.length) {
+          throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
+        }
+      },
     }),
   ],
 });
+
+function html(url) {
+  return `<!doctype html>
+  <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+  
+  <head>
+    <title>
+    </title>
+    <!--[if !mso]><!-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style type="text/css">
+      #outlook a {
+        padding: 0;
+      }
+  
+      body {
+        margin: 0;
+        padding: 0;
+        -webkit-text-size-adjust: 100%;
+        -ms-text-size-adjust: 100%;
+      }
+  
+      table,
+      td {
+        border-collapse: collapse;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+      }
+  
+      img {
+        border: 0;
+        height: auto;
+        line-height: 100%;
+        outline: none;
+        text-decoration: none;
+        -ms-interpolation-mode: bicubic;
+      }
+  
+      p {
+        display: block;
+        margin: 13px 0;
+      }
+    </style>
+    <!--[if mso]>
+          <noscript>
+          <xml>
+          <o:OfficeDocumentSettings>
+            <o:AllowPNG/>
+            <o:PixelsPerInch>96</o:PixelsPerInch>
+          </o:OfficeDocumentSettings>
+          </xml>
+          </noscript>
+          <![endif]-->
+    <!--[if lte mso 11]>
+          <style type="text/css">
+            .mj-outlook-group-fix { width:100% !important; }
+          </style>
+          <![endif]-->
+    <!--[if !mso]><!-->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+    <style type="text/css">
+      @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+    </style>
+    <!--<![endif]-->
+    <style type="text/css">
+      @media only screen and (min-width:480px) {
+        .mj-column-per-100 {
+          width: 100% !important;
+          max-width: 100%;
+        }
+      }
+    </style>
+    <style media="screen and (min-width:480px)">
+      .moz-text-html .mj-column-per-100 {
+        width: 100% !important;
+        max-width: 100%;
+      }
+    </style>
+    <style type="text/css">
+    </style>
+  </head>
+  
+  <body style="word-spacing:normal;">
+    <div style="">
+      <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+      <div style="margin:0px auto;max-width:600px;">
+        <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+          <tbody>
+            <tr>
+              <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;">
+                <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                <div class="mj-column-per-100 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                  <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                    <tbody>
+                      <tr>
+                        <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                          <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">
+                            <h1>Авторизация на сайте DeluxSPA</h1>
+                          </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                          <div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;line-height:1;text-align:left;color:#000000;">Для авторизации на сайте DeluxSPA, пожалуйста, пройдите по <a href="${url}" target="_blank">ссылке</a>.</div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <!--[if mso | IE]></td></tr></table><![endif]-->
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!--[if mso | IE]></td></tr></table><![endif]-->
+    </div>
+  </body>
+  
+  </html>`;
+}
