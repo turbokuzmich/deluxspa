72a076e320d21ed389ab18f3d3cbc41a19bfc9cf
Kurteev Dmitry <kurteev.d@yandex.ru>
2022-10-04 15:57:17 +0300

    Send signup memails through yandex message queue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,8 @@
 import NextAuth from "next-auth";
-import EmailProvider from "next-auth/providers/email";
-import nodemailer from "nodemailer";
 import sequelize from "../../../lib/backend/sequelize";
+import { sendSignupEmail } from "../../../lib/backend/queue";
 import SequelizeAdapter from "@next-auth/sequelize-adapter";
 import { User } from "../../../lib/backend/sequelize";
-import renderEmail from "../../../lib/backend/letters/render";
 
 export const authOptions = {
   adapter: SequelizeAdapter(sequelize, {
@@ -14,35 +12,14 @@ export const authOptions = {
     strategy: "jwt",
   },
   providers: [
-    EmailProvider({
-      from: process.env.EMAIL_USER,
-      secret: process.env.NEXTAUTH_SECRET,
-      server: {
-        host: "smtp.yandex.ru",
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
+    {
+      id: "queue",
+      name: "Email",
+      type: "email",
+      async sendVerificationRequest({ identifier, url }) {
+        await sendSignupEmail(identifier, url);
       },
-      async sendVerificationRequest({ identifier, url, provider }) {
-        const transport = nodemailer.createTransport(provider.server);
-
-        const result = await transport.sendMail({
-          to: identifier,
-          from: process.env.EMAIL_SENDER,
-          subject: "Авторизация на сайте DeluxSPA",
-          text: `Для авторизации на сайте DeluxSPA, пожалуйста, перейдите по ссылке ${url}`,
-          html: await renderEmail("signup", { url }),
-        });
-
-        const failed = result.rejected.concat(result.pending).filter(Boolean);
-        if (failed.length) {
-          throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
-        }
-      },
-    }),
+    },
   ],
 };
 
