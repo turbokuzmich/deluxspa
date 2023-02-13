a0b4e829c628f9ddea14ea7245fa482afd716404
turbokuzmich <kurteev.d@yandex.ru>
2023-02-13 18:24:07 +0300

    Admin web app WIP

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,3 +1,41 @@
+import Script from "next/script";
+import Typography from "@mui/material/Typography";
+import { useCallback, useState } from "react";
+import api from "../../lib/frontend/api";
+
 export default function Admin() {
-  return <div>Одминка</div>;
+  const [status, setStatus] = useState("initial");
+
+  const onApiLoaded = useCallback(async () => {
+    try {
+      await api.get("/api/admin/auth", {
+        params: Telegram.WebApp.initDataUnsafe,
+      });
+
+      setStatus("authorized");
+    } catch (error) {
+      setStatus("unauthorized");
+    }
+  }, [setStatus]);
+
+  return (
+    <>
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="afterInteractive"
+        onLoad={onApiLoaded}
+      />
+      {status === "initial" ? (
+        <Typography>Проверяю авторизацию</Typography>
+      ) : null}
+      {status === "unauthorized" ? (
+        <Typography>Вы не имеете права тут находиться</Typography>
+      ) : null}
+      {status === "authorized" ? (
+        <Typography>
+          Добро пожаловать, {Telegram.WebApp.initDataUnsafe.user.first_name}
+        </Typography>
+      ) : null}
+    </>
+  );
 }
