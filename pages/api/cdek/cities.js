f870a687f31de5a9a6fd647a9d66fb802146d115
turbokuzmich <kurteev.d@yandex.ru>
2023-03-02 20:15:07 +0300

    grpc sdek microservice test

--- pages/api/cdek/cities.js
+++ pages/api/cdek/cities.js
@@ -1,24 +1,8 @@
-import { Op } from "sequelize";
-import { City } from "../../../lib/backend/sequelize";
 import get from "lodash/get";
+import { getCdekCities } from "../../../lib/backend/api";
 
 export default async function cities(req, res) {
   const title = get(req, "query.title", "").trim();
 
-  const suggestions = await City.findAll({
-    where: {
-      [Op.or]: [
-        { city: { [Op.like]: `%${title}%` } },
-        { region: { [Op.like]: `%${title}%` } },
-      ],
-      count: {
-        [Op.gt]: 0,
-      },
-      confirmed: true,
-    },
-    limit: 10,
-    order: [["count", "DESC"]],
-  });
-
-  return res.status(200).json(suggestions.map((city) => city.mapData));
+  res.status(200).json(await getCdekCities(title));
 }
