6b46c0104702f405c1d25e34c910fb191d6f9938
turbokuzmich <kurteev.d@yandex.ru>
2023-03-06 14:29:51 +0300

    Fix

--- pages/api/admin/orders.js
+++ pages/api/admin/orders.js
@@ -1,14 +1,29 @@
 import { Order, OrderItem } from "../../../lib/backend/sequelize";
 import omit from "lodash/omit";
+import get from "lodash/get";
 import { restricted } from "../../../lib/middleware/admin";
 
-export default restricted(async function orders(_, res) {
-  const orders = (
-    await Order.findAll({
-      order: [["createdAt", "DESC"]],
-      include: [OrderItem],
-    })
-  ).map((order) => omit(order.toJSON(), ["hmac", "paymentReturnUrl"]));
+export default async function orders(req, res) {
+  const id = get(req, ["query", "id"], null);
 
-  res.status(200).json(orders);
-});
+  if (id) {
+    const order = await Order.findByPk(id, {
+      include: [OrderItem],
+    });
+
+    if (order) {
+      res.status(200).json(omit(order.toJSON(), ["hmac", "paymentReturnUrl"]));
+    } else {
+      res.status(404).json();
+    }
+  } else {
+    const orders = (
+      await Order.findAll({
+        order: [["createdAt", "DESC"]],
+        include: [OrderItem],
+      })
+    ).map((order) => omit(order.toJSON(), ["hmac", "paymentReturnUrl"]));
+
+    res.status(200).json(orders);
+  }
+}
